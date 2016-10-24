dd289f63f712cf3e7fe9d3240ca7f4fe2e67a8bd
Alex Spencer <alex@alexspencer.net>
2016-10-24 19:42:36 -0400

    make 1992 again

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Welcome to 1999...Yay alerts!");
+var response = prompt("What is your favorite flavor of icecream?")
+
+if(response == 'vanilla') {
+  alert("That is correct! Well done!");
+} else {
+  alert("You are wrong. Vanilla is the best because you can add anything to it!");
+}
